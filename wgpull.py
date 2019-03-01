6d00e40b218dc427a72f55ca00f35df0370e4886
Drew Abbott <abbotta4@gmail.com>
2019-03-01 02:00:52 -0500

    Initial Commit
    wgpull.py is a Python3 script that pulls the first random wallpaper off
    of wallhaven.cc when searching for the #nature hashtag with some filters
    enabled. The uri is hardcoded for now and the script always saves the
    pulled wallpaper to wallpaper.XXX.

---
+++ wgpull.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+uri = 'https://alpha.wallhaven.cc/search?q=%23nature&categories=100&purity=100&atleast=2560x1440&sorting=random&order=desc&page=3'
+
+try:
+    r = requests.get(uri)
+    if r.status_code is not 200:
+        raise Exception('Bad request')
+except Exception as e:
+    print(e)
+    exit(1)
+
+soup = bs(r.text, 'html.parser')
+uri = soup.main.ul.li.a['href']
+
+try:
+    r = requests.get(uri)
+    if r.status_code is not 200:
+        raise Exception('Bad request')
+except Exception as e:
+    print(e)
+    exit(1)
+
+soup = bs(r.text, 'html.parser')    
+uri = 'https://' + soup.main.img['src'][2:]
+
+try:
+    r = requests.get(uri)
+    if r.status_code is not 200:
+        raise Exception('Bad request')
+except Exception as e:
+    print(e)
+    exit(1)
+
+filename = './wallpaper.' + uri[-3:]
+print('saving ' + uri + ' to ' + filename)
+with open(filename , 'wb') as f:
+    f.write(r.content)
