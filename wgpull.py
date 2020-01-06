3be67fdfccf9f3d7e1d2bf075c714958be82f79a
Andrew Abbott <drewa@motorola.com>
2020-01-06 12:21:51 -0600

    Add get function
    Create get function to wrap up request handling to condense code and
    improve readability.
    Add comments to explain individual requests.

--- wgpull.py
+++ wgpull.py
@@ -5,37 +5,27 @@ from bs4 import BeautifulSoup as bs
 
 uri = 'https://wallhaven.cc/search?q=nature&categories=100&purity=100&atleast=2560x1440&sorting=random&order=desc'
 
-try:
-    r = requests.get(uri)
-    if r.status_code is not 200:
-        raise Exception('Bad request')
-except Exception as e:
-    print(e)
-    exit(1)
-
+# requests.get with a status_code exception
+def get(uri):
+    try:
+        r = requests.get(uri)
+        if r.status_code is not 200:
+            raise Exception('Bad request')
+    except Exception as e:
+        print(e)
+        exit(1)
+    return r
+    
+r = get(uri) # Load a results page
 soup = bs(r.text, 'html.parser')
 uri = soup.main.ul.li.a['href']
-
-try:
-    r = requests.get(uri)
-    if r.status_code is not 200:
-        raise Exception('Bad request')
-except Exception as e:
-    print(e)
-    exit(1)
-
+r = get(uri) # get the first link on the resutls page
 soup = bs(r.text, 'html.parser')    
 uri = 'https://' + soup.main.img['src'][2:]
-
-try:
-    r = requests.get(uri)
-    if r.status_code is not 200:
-        raise Exception('Bad request')
-except Exception as e:
-    print(e)
-    exit(1)
-
+r = get(uri) # download the source image
 filename = './wallpaper.' + uri[-3:]
+
+# save the file
 print('saving ' + uri + ' to ' + filename)
 with open(filename , 'wb') as f:
     f.write(r.content)
